3cdecd51025ae971e7468836a93fbd5f74d01a05
moralfager <moralfager@mail.ru>
2025-10-07 00:49:02 +0500

    fix: ignore node_modules, dist and .env

--- dist/bmz/dto/create-area-price.dto.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class CreateAreaPriceDto {
-    minArea: number;
-    maxArea: number;
-    minWallThickness: number;
-    maxWallThickness: number;
-    basePricePerSquareMeter: number;
-}
--- dist/currency-settings/currency-settings.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { CurrencySettingsService } from './currency-settings.service';
-import { CurrencySettings } from './entities/currency-settings.entity';
-import { UpdateCurrencySettingsDto } from './dto/update-currency-settings.dto';
-export declare class CurrencySettingsController {
-    private readonly currencySettingsService;
-    constructor(currencySettingsService: CurrencySettingsService);
-    getSettings(): Promise<CurrencySettings>;
-    updateSettings(updateData: UpdateCurrencySettingsDto): Promise<CurrencySettings>;
-}
